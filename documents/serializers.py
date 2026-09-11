fe1ab5440d3aee6f0d78645e54ba76d77407763b
Jose Gonzalez Gonzalez <josegonzalezglez1964@gmail.com>
2026-09-11 08:53:35 +0100

    Add read_only_fields to DocumentSerializer

--- documents/serializers.py
+++ documents/serializers.py
@@ -5,3 +5,4 @@ class DocumentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Document
         fields = ['id', 'doc_type', 'title', 'merchant_name', 'created_at', 'updated_at']
+        read_only_fields = ['id', 'created_at', 'updated_at']
