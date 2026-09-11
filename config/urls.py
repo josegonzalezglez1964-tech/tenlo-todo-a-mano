3b30c0de6419c46af21fb5bd908441e41e71322c
Jose Gonzalez Gonzalez <josegonzalezglez1964@gmail.com>
2026-09-11 14:00:56 +0100

    Update home view response and add name to URL path

--- config/urls.py
+++ config/urls.py
@@ -4,10 +4,10 @@ from django.http import HttpResponse
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 def home(request):
-    return HttpResponse("API funcionando")
+    return HttpResponse("<h1>API funcionando</h1>")
 
 urlpatterns = [
-    path('', home),
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
     path('api/', include('documents.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
