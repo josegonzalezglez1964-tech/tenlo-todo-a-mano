110ab4267ac9bb0460c8b5c276a18a021b2b9360
Jose Gonzalez Gonzalez <josegonzalezglez1964@gmail.com>
2026-09-11 08:49:02 +0100

    Refactor URLs to use DefaultRouter for DocumentViewSet

--- documents/urls.py
+++ documents/urls.py
@@ -1,14 +1,12 @@
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import DocumentViewSet
+
+app_name = 'documents'
 
 router = DefaultRouter()
 router.register(r'documents', DocumentViewSet, basename='documents')
 
-urlpatterns = router.urls
-from django.urls import path
-from . import views
-
-app_name = 'documents'
-
 urlpatterns = [
+    path('', include(router.urls)),
 ]
