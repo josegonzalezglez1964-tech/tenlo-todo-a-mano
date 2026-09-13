c09d68f6b439a5c2cbc5866ea1891c20f414e1c6
Jose Gonzalez Gonzalez <josegonzalezglez1964@gmail.com>
2026-09-13 16:29:22 +0100

    Add 'owner' field to DocumentSerializer

--- documents/serializers.py
+++ documents/serializers.py
@@ -8,11 +8,11 @@ class DocumentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Document
         fields = [
-            'id', 'upload_key', 'doc_type', 'title', 'merchant_name',
+            'id', 'upload_key', 'owner', 'doc_type', 'title', 'merchant_name',
             'date', 'time', 'total', 'taxes', 'currency', 'payment_method',
             'category', 'status', 'file',
             'created_at', 'updated_at',
         ]
-        read_only_fields = ['id', 'upload_key', 'created_at', 'updated_at']
+        read_only_fields = ['id', 'upload_key', 'owner', 'created_at', 'updated_at']
 EOF
 cat documents/serializers.py
