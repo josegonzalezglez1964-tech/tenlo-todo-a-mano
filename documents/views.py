06bab1db162451f7fb87253fc66da4281b5b3ad6
Jose Gonzalez Gonzalez <josegonzalezglez1964@gmail.com>
2026-09-13 16:28:27 +0100

    Add authentication to DocumentViewSet

--- documents/views.py
+++ documents/views.py
@@ -1,9 +1,21 @@
+cat > documents/views.py << 'EOF'
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.parsers import MultiPartParser, FormParser, JSONParser
+
 from .models import Document
 from .serializers import DocumentSerializer
 
+
 class DocumentViewSet(ModelViewSet):
-    queryset = Document.objects.all().order_by('-created_at')
     serializer_class = DocumentSerializer
     parser_classes = [JSONParser, FormParser, MultiPartParser]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Document.objects.filter(owner=self.request.user).order_by('-created_at')
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+EOF
+cat documents/views.py
